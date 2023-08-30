01d86ad9ed016834b8f28bab893edeaa0b77fa3d
Karina Krisan <99372223+KarinaKrisan@users.noreply.github.com>
2023-08-30 11:23:06 -0300

    variaveisetiposdedados.py

---
+++ variaveisetiposdedados.py
@@ -0,0 +1,10 @@
+nome = "Luana" #string
+idade = 25 #int
+#altura = 1.68 #float
+#is_male = False / True #bool
+sou_estudante = "Ciências Contabeis"
+
+print(f"Meu nome é {nome} tenho {idade} anos de idade e sou estudante de {sou_estudante}")
+
+
+
