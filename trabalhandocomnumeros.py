b27fef00078ea34e4e088486b4370a92419fa51c
Karina Krisan <99372223+KarinaKrisan@users.noreply.github.com>
2023-08-30 11:23:50 -0300

    trabalhandocomnumeros.py

---
+++ trabalhandocomnumeros.py
@@ -0,0 +1,55 @@
+#import de biblioteca
+
+import math
+
+##
+
+num1 = 4 #int
+num2 = 1.5 #float
+
+#verificação de tipo de dado
+#print(type(num1))
+#print(type(num2))
+
+#conversão de dado
+#a = float (num1)
+#print(a)
+#print(type(a))
+#b = int (num2)
+#print(b)
+#print(type(b))
+
+#calculos
+#print(num1 + num2)
+#print(num1 - num2)
+#print(num1 * num2)
+#print(num1 / num2)
+#print(num1 ** num2)
+#print(num1 // num2)
+
+#print((3+5)*(7+3))
+#print(3+5*7+3)
+
+#Retorna o número absoluto
+#print(abs(-15))
+
+#Exponenciação
+#print(pow(3,3))
+
+#Max
+#print(max(1,85, 17, -55,5))
+
+#Min
+#print(min(1,85, 17, -55,5))
+
+#round
+#print(round(8.8))
+
+#floor arredonda para baixo
+#print(math.floor(8.8))
+
+#ceil arredonda para cima
+#print(math.ceil(8.8))
+
+#raiz quadrada
+print(math.sqrt(10))
